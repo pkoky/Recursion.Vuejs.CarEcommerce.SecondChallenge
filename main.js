e44b2c3a23737cb1b861d6ab2b4281b531969ed9
pkoky <sscsk2224@gmail.com>
2021-10-18 15:53:56 +0900

    Add [Car Class] to Js

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 class Car {
+  constructor(carModel, category, price, imgUrl, registrationDate) {
+    this.carModel = carModel;
+    this.category = category;
+    this.price = price;
+    this.imgUrl = imgUrl;
+    this.registrationDate = registrationDate;
+  }
 } 
+
